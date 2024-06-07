94498dac03429895fd76b4f606db977a54ba8219
Magan Sundar Oram <137607000+Magan-Sundar-Oram@users.noreply.github.com>
2024-06-07 23:26:48 +0530

    Update url.js

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,1 +1,2 @@
-export const baseUrl=`https://paytm-basic-backend.onrender.com`
+// export const baseUrl=`https://paytm-basic-backend.onrender.com`
+export const baseUrl=`https://paytm-basic-backend-brown.vercel.app`
