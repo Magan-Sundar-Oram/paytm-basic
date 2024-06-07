886e6a4b929136e10ad2cee35a1d889033f74705
Magan Sundar Oram <137607000+Magan-Sundar-Oram@users.noreply.github.com>
2024-06-07 23:31:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const cors=require('cors')
 
 app.use(cors(
     {
-        origin:["https://paytm-basic-v1.vercel.app"],
+        origin: 'https://paytm-basic-v1.vercel.app',
         methods:["POST","GET","PUT"],
         credentials:true,
     }
