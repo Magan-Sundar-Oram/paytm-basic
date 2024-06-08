782dcd763ce4fc113f840a5f78c4a25a72cefe0d
Magan Sundar Oram <137607000+Magan-Sundar-Oram@users.noreply.github.com>
2024-06-08 09:59:12 +0530

    Update Users.jsx

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -3,11 +3,10 @@ import Button from './Button';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import {baseUrl} from '../url';
 
 const Users = () => {
     const [users, setUsers] = useState([]);
-    const [filter, setFilter] = useState("")
+    const [filter, setFilter] = useState("F")
     const [loggedInUser, setLoggedInUser] = useState(null)
 
     const { isAuthenticated } = useAuth();
@@ -15,7 +14,7 @@ const Users = () => {
     useEffect(() => {
         const fetchLoggedInUser = async () => {
             try {
-                const response = await axios.get(`${baseUrl}/api/v1/user/inuser`, {
+                const response = await axios.get("http://localhost:3000/api/v1/user/inuser", {
                     headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
                 });
                 setLoggedInUser(response.data.userData);
@@ -33,7 +32,7 @@ const Users = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get(`${baseUrl}/api/v1/user/bulk?filter=${filter}`);
+                const response = await axios.get("http://localhost:3000/api/v1/user/bulk?filter=" + filter);
                 setUsers(response.data.users)
             } catch (error) {
                 console.log("Error while fetching data" + error)
@@ -52,14 +51,15 @@ const Users = () => {
 
     return (
         <>
-            <div className="font-bold mt-6 text-lg">
-                Users
+            <div className="font-medium mt-6 text-xl">
+                Users 👤
             </div>
             <div className="my-2">
                 <input onChange={(e) => {
                     setFilter(e.target.value)
                 }}
-                    type="text" placeholder="Search users..." className="w-full px-2 py-1 border rounded border-slate-200"></input>
+                    type="text" placeholder="Search users..." 
+                    className="w-full px-2 py-3 border rounded-md border-slate-200"/>
             </div>
             <div>
                 {filteredUsers.map(user => <User key={user._id} user={user} />)}
@@ -71,21 +71,21 @@ function User({ user }) {
 
     const navigate = useNavigate();
 
-    return <div className="flex justify-between p-1 ">
-        <div className="flex">
+    return <div className="shadow-md items-center mb-2 rounded-md md:flex-row flex justify-between p-2 ">
+        <div className="flex items-center">
             <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
                 <div className="flex flex-col justify-center h-full text-xl">
                     {user.username[0]}
                 </div>
             </div>
             <div className="flex flex-col justify-center h-ful">
-                <div>
+                <div className='text-xl'>
                     {user.username}
                 </div>
             </div>
         </div>
 
-        <div className="flex flex-col justify-center h-ful">
+        <div className="flex items-center flex-col justify-center h-full">
             <Button onClick={(e) => {
                 navigate("/sendmoney?id=" + user._id + "&username=" + user.username)
             }}
