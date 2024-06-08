b408a1b694435051f3b9f64a64ccaf4575c3fb07
Magan Sundar Oram <137607000+Magan-Sundar-Oram@users.noreply.github.com>
2024-06-08 16:27:03 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 2s linear infinite',
+        'spin-medium': 'spin 1.5s linear infinite',
+        'spin-fast': 'spin 1s linear infinite',
+      },
+      keyframes: {
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        }
+      }
+    },
   },
   plugins: [],
 }
