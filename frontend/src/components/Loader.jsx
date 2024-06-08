d09c39324f0ea51184d76d9a6a9089f465c51820
Magan-Sundar-Oram <msoram7377@gmail.com>
2024-06-08 22:04:05 +0530

    loader updated

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -2,11 +2,8 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="relative w-16 h-16">
-        <div className="absolute border-4 border-transparent border-t-blue-500 border-r-blue-500 rounded-full w-full h-full animate-spin"></div>
-        <div className="absolute border-4 border-transparent border-b-blue-500 border-l-blue-500 rounded-full w-full h-full animate-spin delay-200"></div>
-      </div>
+    <div className='flex justify-center items-center h-screen'>
+      <div className="loader border-t-4 border-b-4 border-blue-500 rounded-full w-16 h-16 animate-spin"></div>
     </div>
   )
 }
