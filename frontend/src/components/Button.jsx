2e91e6e305373bb271432fd5e32f4f9ad5e7095d
Magan Sundar Oram <137607000+Magan-Sundar-Oram@users.noreply.github.com>
2024-06-08 09:55:24 +0530

    Update Button.jsx

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ label, onClick }) => {
     return (
-        <button onClick={onClick} type='button' className='w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-3 me-2 mb-2'>
+        <button onClick={onClick} type='button' className='w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 md:font-medium rounded-lg text-md px-2 py-2 me-2 mb-2'>
             {label}
         </button>
     )
